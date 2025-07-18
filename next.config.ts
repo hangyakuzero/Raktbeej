dc280763f754c02728b8a3c49d48fecb0a681d6c
Hangyaku <hangyakuzero@gmail.com>
2025-07-19 03:59:57 +0530

    vercelify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.resolve.fallback = {
+      ...(config.resolve.fallback || {}),
+      canvas: false,
+      fs: false, // also helpful for pdfjs
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
